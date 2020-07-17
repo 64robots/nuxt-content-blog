08cbb450cf23e483dbcafb022b326cae8d036d4e
Pepe García <pgarciag93@gmail.com>
2020-07-17 10:27:49 +0200

    Add tailwindcss typography plugin

--- pages/_permalink.vue
+++ pages/_permalink.vue
@@ -25,7 +25,7 @@
           </div>
 
           <div class="mt-6">
-            <nuxt-content :document="post" />
+            <nuxt-content :document="post" class="prose sm:prose lg:prose-lg xl:prose-xl" />
           </div>
         </div>
       </article>
@@ -38,108 +38,3 @@ export default {
   name: 'ViewPost',
 }
 </script>
-<style scoped lang="postcss">
-.nuxt-content {
-  @apply text-base text-gray-900 leading-normal;
-
-  & h1 {
-    @apply text-4xl font-bold leading-7 mb-2;
-  }
-
-  & h2 {
-    @apply text-3xl font-bold leading-6 mb-2;
-  }
-
-  & h3 {
-    @apply text-2xl font-semibold leading-5 mb-1;
-  }
-
-  & h4 {
-    @apply text-xl font-semibold leading-5 mb-1;
-  }
-
-  & h5 {
-    @apply text-lg font-semibold;
-  }
-
-  & strong {
-    @apply font-bold;
-  }
-
-  & em {
-    @apply italic;
-  }
-
-  & del {
-    @apply line-through;
-  }
-
-  & > * + *,
-  & li + li,
-  & li > p + p {
-    @apply mt-6;
-  }
-
-  & ul > li {
-    @apply list-disc;
-  }
-
-  & ol > li {
-    @apply list-decimal;
-  }
-
-  & strong {
-    @apply text-black font-bold;
-  }
-
-  & a[href^='http'],
-  & a[href^='www'] {
-    @apply text-black font-semibold border-b-2 border-black transition ease-in-out duration-200 px-1;
-
-    &:hover {
-      @apply bg-black text-white;
-    }
-  }
-
-  & a[href^='#'] > span.icon {
-    background: url('data:image/svg+xml,<svg fill="none" xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="black"><path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>');
-    @apply bg-no-repeat w-4 h-4 inline-block mr-2;
-  }
-
-  & strong a {
-    @apply font-bold;
-  }
-
-  & p > code {
-    @apply font-mono text-sm inline bg-gray-200 px-2 py-1;
-  }
-
-  & blockquote {
-    @apply border-l-4 border-gray-200 pl-4 italic;
-  }
-
-  & ul,
-  & ol {
-    @apply pl-5;
-    @screen sm {
-      @apply pl-10;
-    }
-  }
-
-  & table {
-    @apply table-auto;
-  }
-
-  & table thead tr th {
-    @apply px-4;
-    @apply py-2;
-    @apply font-bold;
-  }
-
-  & table tbody tr td {
-    @apply px-4;
-    @apply py-2;
-    @apply border;
-  }
-}
-</style>
