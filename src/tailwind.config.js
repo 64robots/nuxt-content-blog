452f863be11a7036f2a44fead508a9a0ccbfa956
Pepe García <pgarciag93@gmail.com>
2020-09-15 20:06:08 +0200

    Add purge css options to tailwind config file

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,9 +7,21 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-}
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "content/**/*.md",
+      path.join(__dirname, "components/**/*.vue"),
+      path.join(__dirname, "layouts/**/*.vue"),
+      path.join(__dirname, "pages/**/*.vue"),
+      path.join(__dirname, "plugins/**/*.js"),
+      "nuxt.config.js",
+    ],
+  },
+};
